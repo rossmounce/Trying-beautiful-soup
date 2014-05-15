647411a2273417db7858e42c48a346a1a9f1e70c
Ross Mounce <ross.mounce@gmail.com>
2014-05-15 18:44:30 +0100

    Done. Have non-interactive version of script developed in the IPython Notebook

---
+++ get-figures.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from bs4 import BeautifulSoup
+import sys, getopt
+
+
+soup = BeautifulSoup (open(sys.argv[1]))
+divfig = soup.find_all('div', class_="fig")
+print(divfig)
+#IMAGE LINKS
+file = open(sys.argv[1]+"-URLs.txt", "wb")
+
+for tr in divfig:
+    for link in tr.find_all('a'):
+        fullink = link.get ('href')
+        print fullink #print in terminal to verify results
+        file.write(fullink+'\n')
+
+file.flush()
+file.close()
+
+#CAPTIONS
+file = open(sys.argv[1]+"-caps.txt", "wb")
+
+for tr in divfig:
+    for link in tr.find_all('p'):
+        plaintext = link.get_text ()
+        oneline = plaintext.replace('\n',' ').replace('          ',' ')
+        print oneline
+        file.write(oneline+'\n')
+
+file.flush()
+file.close()
+
