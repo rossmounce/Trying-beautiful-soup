d037fdbf4a2df930330c3cab78361c688e959a4c
Ross Mounce <ross.mounce@gmail.com>
2014-05-19 13:41:43 +0100

    save doi in each subfolder

--- create_subfolders.sh
+++ create_subfolders.sh
@@ -4,4 +4,6 @@ for f in *.html
 	echo "started on $f"
 	STEM=$(echo $f | sed 's/.....$//g' )
 	mkdir $STEM
+	cmd="grep -m1 'dc:identifier' $f | sed 's/<dc:identifier>info:doi/http:\/\/dx\.doi\.org/g' | sed 's/<\/dc:identifier>//g' > ./$STEM/doi.doi"
+	eval $cmd
 	done
